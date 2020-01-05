0b18f9367eb1f27ec4742b95728287131560ef0c
TShipley <taneymail@gmail.com>
2020-01-05 22:25:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice = (veg1, veg2, protein = meat)
 puts "What a nutritious meal!"
-return "A plate of prYour code here
+return Your code here
 # For output purposes, use "puts" instead of "print" or "p"
